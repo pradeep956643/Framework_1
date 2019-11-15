177244e0103c179142258dcea1a344a9bb07ee8a
Srinivasa <Srinivasa.potu@gmail.com>
2019-11-15 12:01:22 +0530

    Paramettization done through pom.xml

--- Framework/src/test/java/com/learnautomation/testcases/LoginTestCRM.java
+++ Framework/src/test/java/com/learnautomation/testcases/LoginTestCRM.java
@@ -8,20 +8,19 @@ import com.learnautomation.pages.LoginPage;
 
 public class LoginTestCRM extends BaseClass {
 
-	@Test(priority=1)
+	@Test(priority = 1)
 	public void loginApp() {
 
-		logger=report.createTest("Login to CRM");
-		
+		logger = report.createTest("Login to CRM");
+
 		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
 
 		logger.info("Starting Application");
-		
+
 		loginPage.loginToCRM(excel.getStringData("Login", 0, 0), excel.getStringData("Login", 0, 1));
-		
+
 		logger.pass("Login Success");
-		
+
 	}
-	
 
 }
